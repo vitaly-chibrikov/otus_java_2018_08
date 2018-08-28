005cf8ed161e1c148ea948d782380946a9445322
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2018-08-28 16:38:18 +0300

    lottery 2

--- lottery/src/main/java/ru/otus/lottery/MainStream.java
+++ lottery/src/main/java/ru/otus/lottery/MainStream.java
@@ -6,27 +6,15 @@ import java.nio.file.Paths;
 import java.util.Comparator;
 
 /**
- * https://github.com/vitaly-chibrikov/otus_java_2018_06
+ * https://github.com/vitaly-chibrikov/otus_java_2018_08
  * <p>
  * пишите параметром запуска файл source.csv,
  * запускайте.
- * <p>
- * победили те у кого hash > 1_000_000_000
  */
 
 public class MainStream {
     public static void main(String[] args) throws IOException {
-        String salt = "Да прибудет с нами Сила!!!\n" +
-                "\n" +
-                "ytthefirstbornson\u200BБеги Форест, беги\n" +
-                "\n" +
-                "Васильев Дмитрий\u200BА в августе такой же набор с розыгрышем будет?\n" +
-                "\n" +
-                "Victorya Gnevasheva\u200Bhello world\n" +
-                "\n" +
-                "gr_buza\u200Baquila non captat muscas :)\n" +
-                "\n" +
-                "Oleg\u200BА можно где-то проверить есть ли мой ящик в списке?";
+        String salt = "Remind yourself that overconfidence is a slow and insidious killer";
 
         Files.lines(Paths.get(args[0]))
                 .map(String::trim)
